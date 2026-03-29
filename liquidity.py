70ca4a9e3a696e6d8acc30f2ff336809b2d1c3ff
laurenbacall <laurentd.laurentd@gmail.com>
2026-03-29 08:24:53 +0200

    Auto-update: 2026-03-29 08:24

--- liquidity.py
+++ liquidity.py
@@ -395,7 +395,7 @@ ax3.legend(lines + lines2, labels + labels2, loc='upper left')
 # 6-10
 axes[4].plot(p_df.index, get_s('Net_Liq'), color='darkgreen'); format_ax(axes[4], "5. Net Liquidity Path")
 axes[5].plot(p_df.index, get_s('CPI_YoY'), color='purple'); format_ax(axes[5], "6. CPI YoY")
-axes[5].plot(p_df.index, get_s('M2_Real_Growth'), color='purple'); format_ax(axes[6], "6. Real M2 Growth")
+axes[6].plot(p_df.index, get_s('M2_Real_Growth'), color='purple'); format_ax(axes[6], "6. Real M2 Growth")
 axes[7].plot(p_df.index, get_s('Real_10Y_Yield'), color='darkblue'); format_ax(axes[7], "7. Real 10Y Yield")
 axes[8].plot(p_df.index, get_s('Yield_Curve_2s10s'), color='darkgreen'); format_ax(axes[8], "8. Yield Curve")
 axes[9].plot(p_df.index, get_s('USD_Index'), color='navy'); format_ax(axes[9], "9. USD Index")
